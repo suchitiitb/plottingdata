e08c8934e688ebcfb2638872741613fec5ee17e2
suchitiitb <suchit@knowledgefoundry.net>
2015-08-10 18:44:37 +0530

    added 1st test

--- testplotp.py
+++ testplotp.py
@@ -4,21 +4,19 @@ import random
 import unittest
 # import pandas as pd
 
-#from plotp import add_values
-def add_values(a,b):
-    return a + b
- 
-class Testplotp(unittest.TestCase):
-    
+from plotp import add_values 
+
+class Testplotp(unittest.TestCase):    
     @classmethod
     def setUpClass(cls):
+        super(Testplotp, cls).setUpClass()
         cls.a = 1
         cls.b = 2
-        
+       
     def test_add_values(self):
         self.assertEqual(add_values(self.a,self.b),3)
-        
+    
 #comment
       
-
-  
+if __name__ == '__main__':
+    unittest.main()
